80ad9f29d3c3fbaff81e3b2fdd88687fb0cd3cdf
timmykill <marko99thebest@gmail.com>
2020-09-22 15:24:44 +0200

    stuff

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+
 retrieve_ip() {
 	jq -r ".resources[] | select(.name == \"myVps\") | .instances[].attributes.ipv4_address" $TFSTATE
 }
@@ -47,8 +48,10 @@ record_start() {
 
 record_stop() {
 	counter=$1
+	nome="$2"
 	ssh -i $PRIV_KEY root@`retrieve_ip` 'killall -INT ffmpeg'
-	scp -i $PRIV_KEY root@`retrieve_ip`:/home/yolo/reg.mkv $ROOT/regs/${NOME_CORSO}-${ANNO}-${ID}_${counter}.mkv
+	sleep 10s #in case ffmpeg needed this
+	scp -i $PRIV_KEY root@`retrieve_ip`:/home/yolo/reg.mkv "$ROOT/regs/${NOME_CORSO}-${ANNO}-${ID}_${nome}_$(date '+%y%m%d')_${counter}.mkv"
 	cd terraform
 	terraform destroy -var="anno=$ANNO" -var="corso=$NOME_CORSO" -state $TFSTATE -auto-approve
 	cd $ROOT
@@ -72,28 +75,33 @@ export ANSIBLE_HOST_KEY_CHECKING="False"
 
 # create private key
 echo 'y' | ssh-keygen -N "" -q -f $PRIV_KEY
+echo
 
 #get piano for today
 oggi=$(date '+%Y-%m-%d')
 counter=0
-curl -s "https://corsi.unibo.it/laurea/$NOME_CORSO/orario-lezioni/@@orario_reale_json?anno=$ANNO&curricula=&start=$oggi&end=$oggi" | jq -r '.[] | .start + " " + .end + " " + .teams' |\
+curl -s "https://corsi.unibo.it/laurea/$NOME_CORSO/orario-lezioni/@@orario_reale_json?anno=$ANNO&curricula=&start=$oggi&end=$oggi" | jq -r '.[] | .start + " " + .end + " " + .teams + " " + .title' |\
 	while read i; do
+		echo $counter
+		counter=$(($counter + 1))
 		start=$(echo $i | cut -d' ' -f1)
 		end=$(echo $i | cut -d' ' -f2)
 		teams=$(echo $i | cut -d' ' -f3)
+		nome=$(echo $i | cut -d' ' -f4- | tr ' /' '_:')
 		seconds_till_start=$(echo `date -d $start '+%s'` ' - ' `date '+%s'` | bc)
 		link_goodpart=$(echo $teams | grep -oE 'meeting_[^%]+')
 		link="https://teams.microsoft.com/_\#/pre-join-calling/19:${link_goodpart}@thread.v2"
 		seconds_till_end=$(echo `date -d $end '+%s'` ' - ' `date '+%s'` | bc)
+		test $seconds_till_end -gt 0 || echo skipping $nome
 		test $seconds_till_end -gt 0 || continue
 		echo waiting for $seconds_till_start secondi
+		echo per lezione: $nome
 		test $seconds_till_start -gt 0 && sleep $seconds_till_start
 		record_start $link
-		echo waiting for $seconds_till_end secondi
 		seconds_till_end=$(echo `date -d $end '+%s'` ' - ' `date '+%s'` | bc)
+		echo waiting for $seconds_till_end secondi
 		test $seconds_till_end -gt 0 && sleep $seconds_till_end
-		record_stop $counter
-		counter=$(($counter + 1))
+		record_stop $counter "$nome"
 	done 
 
 #TODO delete all the junk left behind
